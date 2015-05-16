530ebc09a49114767662d31c64a0d0eac7ea775d
Simi <smanchanda@Simis-Mac-mini.fios-router.home>
2015-05-16 11:49:21 -0400

    Initial Commit

---
+++ MyPortfolio/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyPortfolio
+//
+//  Created by Simi on 5/16/15.
+//  Copyright (c) 2015 seempal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyPortfolio/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyPortfolio
+//
+//  Created by Simi on 5/16/15.
+//  Copyright (c) 2015 seempal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
